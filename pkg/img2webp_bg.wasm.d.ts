23bf592dcd1d42febad557945f215195a45e51fc
Canmi <9997200@qq.com>
2025-04-08 09:30:07 +0800

    add: pkg

---
+++ pkg/img2webp_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const convert_png_to_webp: (a: number, b: number) => [number, number, number, number];
+export const convert_jpeg_to_webp: (a: number, b: number) => [number, number, number, number];
+export const convert_bmp_to_webp: (a: number, b: number) => [number, number, number, number];
+export const detect_image_format: (a: number, b: number) => [number, number];
+export const process_image_to_webp: (a: number, b: number) => [number, number, number, number];
+export const convert_jpg_to_webp: (a: number, b: number) => [number, number, number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
